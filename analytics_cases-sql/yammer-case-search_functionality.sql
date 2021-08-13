ccce215f9e9965c0188b775281bf4a1304c14355
John Antolik <87459650+johnantolik@users.noreply.github.com>
2021-08-12 19:17:04 -0700

    new

---
+++ analytics_cases-sql/yammer-case-search_functionality.sql
@@ -0,0 +1,15 @@
+# Data Orientation
+
+SELECT *
+  FROM tutorial.yammer_users
+
+SELECT *
+  FROM tutorial.yammer_events
+
+SELECT *
+  FROM tutorial.yammer_emails
+
+SELECT * 
+  FROM tutorial.yammer_experiments
+
+
